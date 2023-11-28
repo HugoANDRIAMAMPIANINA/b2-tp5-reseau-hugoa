a52e55c989fbdfc5120fdb2be34867213b41e753
ahugo <hugo.andriamampianina@ynov.com>
2023-11-28 11:08:23 +0100

    update

--- test_client.py
+++ test_client.py
@@ -3,7 +3,7 @@ from sys import exit
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(('127.0.0.1', 12345))
+s.connect(('10.1.1.11', 12345))
 
 try:
     user_number = int(input("Entrez un nombre : "))
