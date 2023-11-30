b7e0af09d655606360d6d2a582f0888741008b99
ahugo <hugo.andriamampianina@ynov.com>
2023-11-30 12:20:20 +0100

    update

--- tp5_web_serv_1.py
+++ tp5_web_serv_1.py
@@ -9,7 +9,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        
+        index_content = "<h1>Hello je suis un serveur HTTP</h1>"
+        self.wfile.write(index_content.encode("utf-8"))
         
 def run():
     port = 8080
