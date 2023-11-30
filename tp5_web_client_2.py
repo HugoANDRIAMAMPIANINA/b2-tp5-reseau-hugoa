2fb6b919f33f5dd9d41dc360ff0d67520d42ab11
ahugo <hugo.andriamampianina@ynov.com>
2023-11-30 12:39:49 +0100

    update

--- tp5_web_client_2.py
+++ tp5_web_client_2.py
@@ -2,7 +2,7 @@ import socket
 from re import compile
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.1.1.11', 9999))
+s.connect(('10.1.1.11', 8080))
 
 user_input = input("")
 input_byte_len = int.to_bytes(len(user_input),4, byteorder='big')
