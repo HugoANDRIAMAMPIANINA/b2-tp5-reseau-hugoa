841281fd931a861f533da285359ef6d23e565616
ahugo <hugo.andriamampianina@ynov.com>
2023-11-30 12:09:52 +0100

    update

---
+++ tp5_web_serv_1.py
@@ -0,0 +1,28 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+host_name = "localhost"
+server_port = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((host_name, server_port), MyServer)
+    print(f"Server started http://{host_name}:{server_port}")
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
