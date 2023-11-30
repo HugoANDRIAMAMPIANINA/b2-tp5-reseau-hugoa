22f79dfbe0485637257245511fbc67e835d7d248
ahugo <hugo.andriamampianina@ynov.com>
2023-11-30 12:18:24 +0100

    update

--- tp5_web_serv_1.py
+++ tp5_web_serv_1.py
@@ -4,25 +4,19 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 host_name = "10.1.1.11"
 server_port = 8080
 
-class MyServer(BaseHTTPRequestHandler):
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        
+def run():
+    port = 8080
+    server_address = ('', port)
+    web_server = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print(f"Serveur lancé sur le port :{port}")
+    web_server.serve_forever()
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((host_name, server_port), MyServer)
-    print(f"Server started http://{host_name}:{server_port}")
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+if __name__ == '__main__':
+    run()
